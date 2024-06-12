2f82932b76e989dbd6c768afbbe810054a4ffe17
yuki1229h10 <yuki1229.h10@i.softbank.jp>
2024-06-12 20:53:37 +0900

    update

--- Spring Boot/AOPSample/src/main/java/com/example/AOPSample/AopSampleApplication.java
+++ Spring Boot/AOPSample/src/main/java/com/example/AOPSample/AopSampleApplication.java
@@ -1,13 +1,25 @@
 package com.example.AOPSample;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.example.AOPSample.service.TargetService;
 
 @SpringBootApplication
 public class AopSampleApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AopSampleApplication.class, args);
+		SpringApplication.run(AopSampleApplication.class, args).getBean(AopSampleApplication.class).exe();
+	}
+
+	@Autowired
+	private TargetService targetService;
+
+	private void exe() {
+		targetService.sayHello("Ken");
+		System.out.println("------");
+		targetService.sayGoodbye("Ryu");
 	}
 
 }
--- Spring Boot/AOPSample/src/main/java/com/example/AOPSample/aop/LoggingAspect.java
+++ Spring Boot/AOPSample/src/main/java/com/example/AOPSample/aop/LoggingAspect.java
@@ -4,31 +4,47 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class LoggingAspect {
 
-	@Before("execution(* com.example.AOPSample.service.TargetService.*(..))")
+	//	@Before("execution(* com.example.AOPSample.service.TargetService.*(..))")
 	public void beforeAdvice(JoinPoint joinPoint) {
 		LocalDateTime startTime = LocalDateTime.now();
 		String formattedTime = startTime.format(DateTimeFormatter.ofPattern("HH:mm:ss:SSS"));
 		System.out.println("before");
-		System.out.println("bedore method:" + joinPoint.getSignature());
+		System.out.println("before method:" + joinPoint.getSignature());
 		System.out.println("start method:" + formattedTime);
 	}
 
-	@After("execution(* com.example.AOPSample.service.TargetService.*(..))")
+	//	@After("execution(* com.example.AOPSample.service.TargetService.*(..))")
 	public void afterAdvice(JoinPoint joinPoint) {
 		LocalDateTime endTime = LocalDateTime.now();
 		String formattedTime = endTime.format(DateTimeFormatter.ofPattern("HH:mm:ss:SSS"));
 		System.out.println("after");
 		System.out.println("after method:" + joinPoint.getSignature());
-		System.out.println("finish method:" + formattedTime);
+		System.out.println("end method:" + formattedTime);
+	}
+
+	@Around("execution(* com.example.AOPSample.service.TargetService.*(..))")
+	public Object aroundAdvice(ProceedingJoinPoint joinPoint) throws Throwable {
+		long startTime = System.currentTimeMillis();
+		System.out.println("around:before");
+		System.out.println("target");
+		System.out.println("class:" + joinPoint.getSignature().getDeclaringTypeName());
+		System.out.println("method:" + joinPoint.getSignature().getName());
+
+		Object result = joinPoint.proceed();
+
+		System.out.println("around:after");
+		long elapsedTime = System.currentTimeMillis() - startTime;
+		System.out.println("method execution time:" + elapsedTime);
+		return result;
 	}
 
 }
