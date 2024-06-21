18c190e8eb38dd9b186a203444fb582dfa5084cf
yuki1229h10 <yuki1229.h10@i.softbank.jp>
2024-06-21 21:02:14 +0900

    add

--- Spring Boot/PathVariableSample/src/main/java/com/example/PathVariableSample/controller/PathValiableController.java
+++ Spring Boot/PathVariableSample/src/main/java/com/example/PathVariableSample/controller/PathValiableController.java
@@ -2,6 +2,8 @@ package com.example.PathVariableSample.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class PathValiableController {
@@ -9,5 +11,37 @@ public class PathValiableController {
 	@GetMapping("show")
 	public String showView() {
 		return "show";
+	}
+
+	@GetMapping("/function/{no}")
+	public String selectFunction(@PathVariable Integer no) {
+		String view = null;
+		switch (no) {
+		case 1:
+			view = "pathvaliable/function1";
+			break;
+		case 2:
+			view = "pathvaliable/function2";
+			break;
+		case 3:
+			view = "pathvaliable/function3";
+			break;
+		}
+		return view;
+	}
+
+	@PostMapping(value = "send", params = "a")
+	public String showAView() {
+		return "submit/a";
+	}
+
+	@PostMapping(value = "send", params = "b")
+	public String showBView() {
+		return "submit/b";
+	}
+
+	@PostMapping(value = "send", params = "c")
+	public String showCView() {
+		return "submit/c";
 	}
 }
---
+++ Spring Boot/ValidationSample/src/test/java/com/example/ValidationSample/ValidationSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ValidationSample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidationSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
